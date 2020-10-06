90ff8e7c2699ca66acab07a8b35c03d94f082b12
Csoór Péter Frank <c80ld7@stud.uni-obuda.hu>
2020-10-06 13:06:10 +0200

    new project created

---
+++ OENIK_PROG3_2020_2_C80LD7/F1.Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace F1.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
