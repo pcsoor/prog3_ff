76168c8f7060e25bc9e1750f13a48f8a7fa0774e
Csoór Péter Frank <c80ld7@stud.uni-obuda.hu>
2020-10-29 23:59:53 +0100

    entities created

---
+++ OENIK_PROG3_2020_2_C80LD7/NBA.Data/Model/Player.cs
@@ -0,0 +1,23 @@
+﻿using Castle.Components.DictionaryAdapter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace NBA.Data.Model
+{
+    [Table("Player")]
+    class Player
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PlayerID { get; set; }
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string Post { get; set; }
+        public int Salary { get; set; }
+        public int Number { get; set; }
+    }
+}
