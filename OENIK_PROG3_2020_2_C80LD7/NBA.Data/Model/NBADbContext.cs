25b57ab0193a8f84dc549b25475d27207f37583a
Csoór Péter Frank <c80ld7@stud.uni-obuda.hu>
2020-10-30 00:00:12 +0100

    Database created

---
+++ OENIK_PROG3_2020_2_C80LD7/NBA.Data/Model/NBADbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBA.Data.Model
+{
+    class NBADbContext : DbContext
+    {
+        public NBADbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public NBADbContext(DbContextOptions<NBADbContext> options) : base(options) {}
+
+        public virtual DbSet<Player> Player { get; set; }
+        
+        public virtual DbSet<Teams> Teams{ get; set; }
+
+        public virtual DbSet<Series> Series{ get; set; }
+
+        public virtual DbSet<TeamStats> TeamStats{ get; set; }
+
+        public virtual DbSet<PlayerStats> PlayerStats{ get; set; }
+
+        /// <inheritdoc/>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=|DataDirectory|\NBADatabase.mdf; Integrated Security = True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Player p0 = new Player() { PlayerID=0, Name="Lebron James", Birth='1984.12.30'}
+        }
+
+    }
+}
