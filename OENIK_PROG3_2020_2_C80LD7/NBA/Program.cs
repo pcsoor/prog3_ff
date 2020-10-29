4d7e96f0a4f8c822c60d72389cd718dc11758339
Csoór Péter Frank <c80ld7@stud.uni-obuda.hu>
2020-10-29 11:08:55 +0100

    project created

---
+++ OENIK_PROG3_2020_2_C80LD7/NBA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NBA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
