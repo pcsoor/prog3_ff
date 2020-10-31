82bea8e30e5c96526eaf319f15323d16cbc7f740
Csoór Péter Frank <c80ld7@stud.uni-obuda.hu>
2020-10-31 19:03:43 +0100

    Logic layer added

---
+++ OENIK_PROG3_2020_2_C80LD7/NBA.Logic/ITeamLogic.cs
@@ -0,0 +1,12 @@
+﻿using NBA.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBA.Logic
+{
+    public interface ITeamLogic
+    {
+        Teams GetOneTeam(int id);
+    }
+}
---
+++ OENIK_PROG3_2020_2_C80LD7/NBA.Logic/TeamLogic.cs
@@ -0,0 +1,23 @@
+﻿using NBA.Data.Model;
+using NBA.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBA.Logic
+{
+    public class TeamLogic : ITeamLogic
+    {
+        ITeamsRepository teamRepo;
+
+        public TeamLogic(ITeamsRepository teamRepo)
+        {
+            this.teamRepo = teamRepo;
+        }
+
+        public Teams GetOneTeam(int id)
+        {
+            return teamRepo.GetOne(id);
+        }
+    }
+}
