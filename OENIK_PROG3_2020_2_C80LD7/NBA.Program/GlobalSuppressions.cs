9ea97763b4d5dd1259fa97b2a8c619521833b4c6
Csoór Péter Frank <c80ld7@stud.uni-obuda.hu>
2020-12-02 01:15:25 +0100

    some warning got supressed in file

--- OENIK_PROG3_2020_2_C80LD7/NBA.Program/GlobalSuppressions.cs
+++ OENIK_PROG3_2020_2_C80LD7/NBA.Program/GlobalSuppressions.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="GlobalSuppressions.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+﻿// <copyright file="GlobalSuppressions.cs" company="C80LD7">
+// Copyright (c) C80LD7. All rights reserved.
 // </copyright>
 
 using System.Diagnostics.CodeAnalysis;
@@ -15,3 +15,9 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "<Pending>", Scope = "member", Target = "~M:NBA.Program.NBAProgram.Main(System.String[])")]
 [assembly: SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>", Scope = "member", Target = "~M:NBA.Program.SimpleMenu.DeletePlayer(NBA.Logic.PlayerLogic)")]
 [assembly: SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>", Scope = "member", Target = "~M:NBA.Program.SimpleMenu.DeletePlayer(NBA.Logic.PlayerLogic)")]
+[assembly: SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>", Scope = "member", Target = "~M:NBA.Program.SimpleMenu.DeleteTeam(NBA.Logic.TeamLogic)")]
+[assembly: SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>", Scope = "member", Target = "~M:NBA.Program.SimpleMenu.UpdatePlayerSalary(NBA.Logic.PlayerLogic)")]
+[assembly: SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>", Scope = "member", Target = "~M:NBA.Program.SimpleMenu.UpdatePlayerSalary(NBA.Logic.PlayerLogic)")]
+[assembly: SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>", Scope = "member", Target = "~M:NBA.Program.SimpleMenu.ChangeTeamName(NBA.Logic.TeamLogic)")]
+[assembly: SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>", Scope = "member", Target = "~M:NBA.Program.SimpleMenu.ChangeTeamName(NBA.Logic.TeamLogic)")]
+[assembly: SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>", Scope = "member", Target = "~M:NBA.Program.SimpleMenu.DeleteTeam(NBA.Logic.TeamLogic)")]
