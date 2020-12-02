3295cb1ac3fbff640b43d3adc4f8adae494d3389
Csoór Péter Frank <c80ld7@stud.uni-obuda.hu>
2020-12-02 01:13:08 +0100

    header specified with my neptun id

--- OENIK_PROG3_2020_2_C80LD7/NBA.Data/Model/Player.cs
+++ OENIK_PROG3_2020_2_C80LD7/NBA.Data/Model/Player.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="Player.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+﻿// <copyright file="Player.cs" company="C80LD7">
+// Copyright (c) C80LD7. All rights reserved.
 // </copyright>
 
 namespace NBA.Data.Model
