e89c46ced6270243e2795c4c87955ea941b70d07
Csoór Péter Frank <c80ld7@stud.uni-obuda.hu>
2020-12-02 01:11:55 +0100

    test classes created

--- OENIK_PROG3_2020_2_C80LD7/NBA.Logic.Tests/PlayerTests.cs
+++ OENIK_PROG3_2020_2_C80LD7/NBA.Logic.Tests/PlayerTests.cs
@@ -1,7 +1,6 @@
-﻿// <copyright file="Class1.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+﻿// <copyright file="PlayerTests.cs" company="C80LD7">
+// Copyright (c) C80LD7. All rights reserved.
 // </copyright>
-
 [assembly: System.CLSCompliant(false)]
 
 namespace NBA.Logic.Tests
@@ -11,9 +10,9 @@ namespace NBA.Logic.Tests
     using System.Text;
 
     /// <summary>
-    /// Test class.
+    /// Player entity specific tests.
     /// </summary>
-    public class Class1
+    public class PlayerTests
     {
     }
 }
---
+++ OENIK_PROG3_2020_2_C80LD7/NBA.Logic.Tests/TeamStats.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="TeamStats.cs" company="C80LD7">
+// Copyright (c) C80LD7. All rights reserved.
+// </copyright>
+
+namespace NBA.Logic.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Team entity specific tests.
+    /// </summary>
+    public class TeamStats
+    {
+    }
+}
