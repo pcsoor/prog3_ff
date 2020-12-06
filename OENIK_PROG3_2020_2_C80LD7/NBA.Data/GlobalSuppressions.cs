a64966fd86e71a123ebd5b22a4a9049a2ddb5204
Csoór Péter Frank <c80ld7@stud.uni-obuda.hu>
2020-12-06 02:32:26 +0100

    supressed one warning

---
+++ OENIK_PROG3_2020_2_C80LD7/NBA.Data/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="C80LD7">
+// Copyright (c) C80LD7. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "error", Scope = "member", Target = "~P:NBA.Data.Model.Teams.Series")]
