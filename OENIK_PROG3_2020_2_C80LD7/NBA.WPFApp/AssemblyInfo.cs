af691f60ee4e7e1855249e9f5b75f3f836ddad6b
csoorpeterfrank <c80ld7@stud.uni-obuda.hu>
2021-03-14 12:40:59 +0100

    CLSComplian added

--- OENIK_PROG3_2020_2_C80LD7/NBA.WPFApp/AssemblyInfo.cs
+++ OENIK_PROG3_2020_2_C80LD7/NBA.WPFApp/AssemblyInfo.cs
@@ -2,8 +2,10 @@
 // Copyright (c) C80LD7. All rights reserved.
 // </copyright>
 
+using System;
 using System.Windows;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, // where theme specific resource dictionaries are located
     ResourceDictionaryLocation.SourceAssembly)]
+[assembly: CLSCompliant(false)]
