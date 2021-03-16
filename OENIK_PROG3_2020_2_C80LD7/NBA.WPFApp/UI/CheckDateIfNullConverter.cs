9f098b30f8c802a26f443d29b60bf75d17583fd9
csoorpeterfrank <c80ld7@stud.uni-obuda.hu>
2021-03-16 18:35:04 +0100

    date null checker conterter added to ui

---
+++ OENIK_PROG3_2020_2_C80LD7/NBA.WPFApp/UI/CheckDateIfNullConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NBA.WPFApp.UI
+{
+    class CheckDateIfNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var nullable = (Nullable<DateTime>)value;
+
+            if (nullable.HasValue && nullable.Value > DateTime.MinValue)
+            {
+                return nullable.Value.ToShortDateString();
+            }
+
+            return "asd";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
