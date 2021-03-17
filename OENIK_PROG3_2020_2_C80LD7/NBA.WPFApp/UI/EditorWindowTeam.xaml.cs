bc0da50391ec46e5da6caf3475dd2f551b9762b1
csoorpeterfrank <c80ld7@stud.uni-obuda.hu>
2021-03-17 01:25:19 +0100

    team editor window created

---
+++ OENIK_PROG3_2020_2_C80LD7/NBA.WPFApp/UI/EditorWindowTeam.xaml.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="EditorWindowTeam.xaml.cs" company="C80LD7">
+// Copyright (c) C80LD7. All rights reserved.
+// </copyright>
+
+namespace NBA.WPFApp.UI
+{
+    using System.Windows;
+    using NBA.WPFApp.Data;
+    using NBA.WPFApp.VM;
+
+    /// <summary>
+    /// Interaction logic for EditorWindowTeam.xaml.
+    /// </summary>
+    public partial class EditorWindowTeam : Window
+    {
+        private readonly EditorViewModelTeam vM;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EditorWindowTeam"/> class.
+        /// </summary>
+        public EditorWindowTeam()
+        {
+            this.InitializeComponent();
+
+            this.vM = this.FindResource("VM") as EditorViewModelTeam;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EditorWindowTeam"/> class.
+        /// </summary>
+        /// <param name="oldTeam">Player ui entity.</param>
+        public EditorWindowTeam(TeamUI oldTeam)
+            : this()
+        {
+            this.vM.TeamUi = oldTeam;
+        }
+
+        private void OkClick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void CancelClick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
