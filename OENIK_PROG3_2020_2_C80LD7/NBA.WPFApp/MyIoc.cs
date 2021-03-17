5cbefa0097ad140c0485ad095062fa8d8cde14a5
csoorpeterfrank <c80ld7@stud.uni-obuda.hu>
2021-03-17 15:40:28 +0100

    create separate class for custom ioc

---
+++ OENIK_PROG3_2020_2_C80LD7/NBA.WPFApp/MyIoc.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="MyIoc.cs" company="C80LD7">
+// Copyright (c) C80LD7. All rights reserved.
+// </copyright>
+
+namespace NBA.WPFApp
+{
+    using CommonServiceLocator;
+    using GalaSoft.MvvmLight.Ioc;
+
+    /// <summary>
+    /// Custom IoC.
+    /// </summary>
+    public class MyIoc : SimpleIoc, IServiceLocator
+    {
+        /// <summary>
+        /// Gets myIoc instance.
+        /// </summary>
+        public static MyIoc Instance { get; private set; } = new MyIoc();
+    }
+}
